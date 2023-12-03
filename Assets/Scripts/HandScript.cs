3f2b0ea74279135e2bf895aae585a08dc984a8b5
Aaron Gill <aaroncjgill@gmail.com>
2023-12-03 17:41:11 -0500

    npc behavior working - will reach for object
    need sound and ending still and slapping

---
+++ Assets/Scripts/HandScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class HandScript : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField]
+    NPCBehavior parentNPC;
+
+    private void OnMouseDown()
+    {
+    }
+    public void OnPointerClick(PointerEventData pointerEventData)
+    {
+        Debug.Log("Recoil hand back");
+        Invoke("recoilHand", 1f);
+    }
+
+    private void recoilHand()
+    {
+        parentNPC.triggered();
+    }
+}
--- Assets/Scripts/NPCBehavior.cs
+++ Assets/Scripts/NPCBehavior.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class NPCBehavior : MonoBehaviour
 {
     AudioSource audioSource;
+    Transform startPos, endPos;
+
     public enum activeScreen
     {
         up,
@@ -19,66 +20,130 @@ public class NPCBehavior : MonoBehaviour
         breathing, //heavy breathing - handsy - 
         handsy
     }
-
+    actions currentAction = actions.normal;
 
     [SerializeField]//the screen this is set to
     activeScreen currentScreen;
     [SerializeField]
     GameObject hand;
-
+    
+    [SerializeField]
+    Transform handStartPos;
+    [SerializeField]
+    Transform handEndPos;
 
     private void Awake()
     {
         
     }
-
+    private void Start()
+    {
+        float x = Random.Range(1, 3);//10/15 to start off
+        //Debug.Log(x);
+        StartCoroutine(TakePhoneRoutine(30));
+    }
+    private void Update()
+    {
+        checkIfLooking();
+    }
     void checkIfLooking()
     {
         //if player is looking at this screen
         //if they are, and the phone is paused, then reset back to normal
         if (GameManager.instance.activeScreen == currentScreen)
         {
-
+            StartCoroutine(moveAway());
         }
-
     }
 
     IEnumerator TakePhoneRoutine(float seconds)
     {
-        yield return new WaitForSeconds(seconds/2);
+        Debug.Log("TakePhone Called" + seconds + "  " + (seconds / 3));
+        yield return new WaitForSeconds(seconds/3);
+        Debug.Log("breathing - grow bigger");
         //wait a certain amount of time and then play heavy breathing
         //play Heavy breathing
-        audioSource.Play();
-        yield return new WaitForSeconds(seconds/2);
+        //audioSource.Play();
+        currentAction = actions.breathing;
+        growBigger();
+        yield return new WaitForSeconds(seconds/3);
+        Debug.Log("start reaching for phone");
+        //start to reach for phone - should take the same amount of time to reach down
+        StartCoroutine(handMoveBehavior(seconds/3));
+        currentAction = actions.handsy;
+        yield return new WaitForSeconds(seconds/3);
+        //lose game
+        //GameManager.instance.gameEnd();
 
-            
+    }
 
+    public void triggered()
+    {
+        StartCoroutine( moveAway());
     }
 
     IEnumerator moveAway()
     {
+        StopCoroutine(handMoveBehavior(100f));
         StopCoroutine(TakePhoneRoutine(40));
+        Debug.Log("HandMoveCoroutine Stopped - moveaway triggered - behavior reset");
+        StartCoroutine(moveHandBackBehavior(3f));
         yield return new WaitForSeconds(1f);
         //react 
         //move back to normal position
-
         //move hand away if on screen
         //stop heavy breathing
 
+        shrinkSmaller();
+        //audioSource.Stop();
+        currentAction = actions.normal;
+
+        //move hand back to position really quickly
 
         //start new routine
         float sec = Random.Range(15,40);
         StartCoroutine(TakePhoneRoutine(sec));
-
     }
+    IEnumerator handMoveBehavior(float t)
+    {
+        Debug.Log("Starting Hand Move Behavior");
+        float changePos = hand.transform.position.x;
+        float timeElapsed = 0;
+        while (timeElapsed < t)
+        {
+            changePos = EasingFunction.EaseInOutSine( handStartPos.position.x, handEndPos.position.x, timeElapsed / t);//Mathf.Lerp(startValue, endValue, timeElapsed / lerpDuration);
+            hand.transform.position = new Vector3(changePos, handEndPos.position.y, 0);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+        hand.transform.position = handEndPos.position;
+        Debug.Log("HandMoveComplete");
+    }
+    IEnumerator moveHandBackBehavior(float t)
+    {
+        float changePos = hand.transform.position.x;
+        float startHandPosNew = hand.transform.position.x;
+        float timeElapsed = 0;
+        while (timeElapsed < t)
+        {
+            changePos = EasingFunction.EaseOutExpo(startHandPosNew, handStartPos.position.x, timeElapsed / t);//Mathf.Lerp(startValue, endValue, timeElapsed / lerpDuration);
+            hand.transform.position = new Vector3(changePos, handEndPos.position.y, 0);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+        hand.transform.position = handEndPos.position;
+    }
+
+
 
     void growBigger()
     {
-
+        //grows self bigger when breathing
+        transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
     }
     void shrinkSmaller()
     {
-
+        transform.localScale = Vector3.one;
     }
 
 }
