9d042f7363668ebe128d4effcd59c8f377d6cf29
cheaptricksss <dao9828@nyu.edu>
2023-12-03 11:32:05 -0500

    Auto stash before merge of "main" and "origin/main"

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    public List<GameObject> scenesInGame = new List<GameObject>();
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/scene.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scene : MonoBehaviour
+{
+    public int xCoordinateInitial;
+    public int yCoordinateInitial;
+}
